072c2486e9acb9fb709ef2b00d18749d11d6214c
WittYoung <166655917+WittYoung@users.noreply.github.com>
2024-10-29 14:27:16 -0600

    completed ICA 10

--- js/ica_10.js
+++ js/ica_10.js
@@ -1,13 +1,17 @@
-console.log(document.getElementById("heading_2"))
-
 document.getElementById("heading").addEventListener("click", function(e){
     document.getElementById("heading").style.color = "red";  
 })
 
 
 document.getElementById("heading_2").addEventListener("dblclick", function(e){
-    document.getElementById("heading_2").style.color = "blue"
     document.getElementById("heading").innerHTML = "Goodbye <3";
 })
 
-console.log(document.getElementById("heading_2").innerHTML);
+document.getElementById('colorButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'lightblue';
+})
+
+document.getElementById('hoverImage').addEventListener('mouseover', function() {
+    this.src = 'img/IMG_5705.jpg';
+    document.getElementById("hoverImage").InnerHTML = "goodbye chill Monkeys :/";
+});
